fb62330bc97a4b7f1d461fe4eb42c9315cbfccd1
abdullah578 <abdullah.am2000@gmail.com>
2020-07-16 13:23:14 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "../public");
+app.use(express.static(publicPath));
+
+server.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
