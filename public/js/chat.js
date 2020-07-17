c66c3858bdad9c59a525bca6dcdc5e072e75bba8
abdullah578 <abdullah.am2000@gmail.com>
2020-07-16 20:41:04 -0400

    Used socket.io to facilitate duel communication between client and server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,19 @@
-io();
+// const socket = io();
+// socket.on("Connection updated", (count) => {
+//   console.log("I received the event", count);
+// });
+
+// document.getElementById("add").addEventListener("click", () => {
+//   socket.emit("increment");
+// });
+const socket = io();
+socket.on("message", (message) => {
+  console.log(message);
+});
+document.querySelector(".message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputSel = document.querySelector(".message-form input");
+  const value = inputSel.value;
+  inputSel.value = "";
+  socket.emit("sendMessage", value);
+});
--- src/index.js
+++ src/index.js
@@ -10,8 +10,15 @@ const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, "../public");
 app.use(express.static(publicPath));
-io.on("connection", () => {
-  console.log("Connected");
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.broadcast.emit("message", "A new user has joined");
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+  socket.on("disconnect", () => {
+    io.prependListener("User has left");
+  });
 });
 
 server.listen(port, () => {
